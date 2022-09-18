89c2228ca2d87fc26bac032274552eddb0ff6ae5
Anna Karkanitsa <karkanica@gmail.com>
2022-09-18 15:01:10 +0300

    Add project files.

---
+++ WebApplication_Demo01/Controllers/FirstController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication_Demo01.Models;
+namespace WebApplication_Demo01.Controllers
+{
+    public class FirstController : Controller
+    {
+        public string Index()
+        {
+            return "Hello World";
+        }
+        public IActionResult Hello()
+        {
+            ViewBag.Message = "Hello World";
+            return View();
+        }
+        public IActionResult Info()
+        {
+            Person person = new Person();
+            person.Name = "John";
+            person.Age = 18;
+            person.Location = "United States";
+            return View(person);
+        }
+    }
+}
---
+++ WebApplication_Demo01/Models/Person.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApplication_Demo01.Models
+{
+    public class Person
+    {
+        public string Name { get; set; } = String.Empty;
+        public int Age { get; set; }
+        public string Location { get; set; } = String.Empty;
+    }
+}
